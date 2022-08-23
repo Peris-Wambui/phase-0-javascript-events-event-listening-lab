75a450886cf6b0dd946f24555fbb37213750b1cf
Peris-Wambui <wangechi.wambui@student.moringaschool.com>
2022-08-22 23:13:31 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,46 @@
-function addingEventListener() {
+// const input = document.getElementById ('input');
+// input.addingEventListener('click', function(){
+//     alert('I was clicked');
+// });
+
+
+// const input = document.getElementById('input');
+
+// function clickAlert() {
+//   alert('I was clicked!');
+// }
+
+// input.addEventListener('click', clickAlert);
+
+
+// const input = document.getElementById('input');
+// function addingEventListener(){
+//     listener ('I was clicked!');
+// }
+// input.addEventListener('listener', addingEventListener);
+
+
+// const input = document.getElementById('input');
+
+//  input.addingEventListener ('listener', function(){
+//     listener('I was clicked');
+//  });
+
+//  function addingEventListener(){
+//     const input =document.getElementById("input");
+//     input.addEventListener('click, clickEvent');
+//  }
+//  function clickEvent(){
+//     alert('clicked a button');
+//  }
+//  addingEventListener();
+
+ function addingEventListener(){
+    const input = document.getElementById('input');
+
+function clickAlert() {
+  alert('I was clicked!');
 }
+
+input.addEventListener('click', clickAlert);
+ }
